37cd67948151b0236bfcc674b165c50e265d11da
Ana Beatriz Ventura <101345386+Bia613@users.noreply.github.com>
2024-03-21 14:26:34 -0300

    Adicionar arquivos de projeto.

---
+++ MauiListaCompras/App.xaml.cs
@@ -0,0 +1,34 @@
+﻿using MauiListaCompras.Helpers;
+
+namespace MauiListaCompras
+{
+    public partial class App : Application
+    {
+        static SQLiteDatabaseHelper _db;
+
+        public static SQLiteDatabaseHelper Db
+        {
+            get
+            {
+                if(_db == null)
+                {
+                    string path = Path.Combine(
+                        Environment.GetFolderPath(
+                            Environment.SpecialFolder.LocalApplicationData
+                            ), "banco_sqlite_compras.db3"
+                        );
+
+                    _db = new SQLiteDatabaseHelper( path );
+                }
+
+                return _db;
+            }
+        }
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
